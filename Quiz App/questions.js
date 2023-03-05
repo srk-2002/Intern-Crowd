5be6fa002b26f9d33fbfefabde1cfb3af25ac406
Saurabh Khachane <124806655+srk-2002@users.noreply.github.com>
2023-03-05 22:42:18 +0530

    Add files via upload

---
+++ Quiz App/questions.js
@@ -0,0 +1,57 @@
+let questions = [
+    {
+    numb: 1,
+    question: "Ordered lists are ",
+    answer: "similar to the numbered lists created MS Word",
+    options: [
+      "not similar to the bullets created in MS Word",
+      "not similar to the numbered lists created MS Word",
+      "similar to the numbered lists created MS Word",
+      "similar to the bullets created in MS word"
+    ]
+  },
+    {
+    numb: 2,
+    question: "The NOSHADE attribute : ",
+    answer: "displays the line in dark gray",
+    options: [
+      "displays the example in red",
+      "displays the line in dark gray",
+      "displays the line in red",
+      "defines the thickness of the line"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Which control is used when the input from the user may extend to several lines?",
+    answer: "multiline text area",
+    options: [
+      "multiline text area",
+      "check box",
+      "password control",
+      "menu control"
+    ]
+  },
+    {
+    numb: 4,
+    question: "The item present within the angled brackets in an HTML tag is : ",
+    answer: "identifier",
+    options: [
+      "data",
+      "identifier",
+      "tags",
+      "text"
+    ]
+  },
+    {
+    numb: 5,
+    question: "BORDER attributes specifies the :",
+    answer: "thickness of the border around the table",
+    options: [
+      "space between two cells",
+      "space between two borders",
+      "width of the table",
+      "thickness of the border around the table"
+    ]
+  },
+];
